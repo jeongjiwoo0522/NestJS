848f0e598d65a656be0cfb5c465161a408b69a4d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-05 15:15:31 +0900

    20200105 patch method

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -20,5 +20,10 @@ export class MoviesController {
   @Delete("/:id") 
   remove(@Param("id") movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch("/:id")
+  path(@Param("id") movieId: string) {
+    return `This will path a movie with the id: ${movieId}`;
   }
 }
